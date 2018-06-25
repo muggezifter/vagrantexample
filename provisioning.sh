8afcb34f508f11928332f57d155129158d9dce79
Gert <gert@gertrietveld.com>
2018-06-25 22:50:06 +0200

    fix redirect error

--- provisioning.sh
+++ provisioning.sh
@@ -50,7 +50,7 @@ echo "ServerName localhost" > /etc/apache2/httpd.conf
 
 # Setup hosts file
 VHOST=$(cat <<EOF
-    <VirtualHost *:8060>
+    <VirtualHost *:8080>
       DocumentRoot "/var/www/app/public"
       ServerName example.local
       ServerAlias www.example.local
@@ -98,44 +98,14 @@ server {
 		# include /etc/nginx/naxsi.rules
 	}
 
-	# Only for nginx-naxsi used with nginx-naxsi-ui : process denied requests
-	#location /RequestDenied {
-	#	proxy_pass http://127.0.0.1:8080;    
-	#}
+	location /api {
+		proxy_pass http://127.0.0.1:8080;
+	}
 
-	#error_page 404 /404.html;
-
-	# redirect server error pages to the static page /50x.html
-	#
-	#error_page 500 502 503 504 /50x.html;
-	#location = /50x.html {
-	#	root /usr/share/nginx/html;
-	#}
-
-	# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
-	#
-	#location ~ \.php$ {
-	#	fastcgi_split_path_info ^(.+\.php)(/.+)$;
-	#	# NOTE: You should have "cgi.fix_pathinfo = 0;" in php.ini
-	#
-	#	# With php5-cgi alone:
-	#	fastcgi_pass 127.0.0.1:9000;
-	#	# With php5-fpm:
-	#	fastcgi_pass unix:/var/run/php5-fpm.sock;
-	#	fastcgi_index index.php;
-	#	include fastcgi_params;
-	#}
-
-	# deny access to .htaccess files, if Apache's document root
-	# concurs with nginx's one
-	#
-	#location ~ /\.ht {
-	#	deny all;
-	#}
 }
 EOF
 )
-echo "${CONF}" >  /etc/nginx/sites-enabled/example.local
+echo "${CONF}" >  /etc/nginx/sites-enabled/default
 sudo service nginx restart
 
 # ---------------------------------------------------------------------------------------------------------------------
