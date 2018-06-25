47b8f2979a6fc4d44e41302ea136b707e41b3934
Gert <gert@gertrietveld.com>
2018-06-25 20:49:43 +0200

    added nginx pt.1

--- provisioning.sh
+++ provisioning.sh
@@ -50,7 +50,7 @@ echo "ServerName localhost" > /etc/apache2/httpd.conf
 
 # Setup hosts file
 VHOST=$(cat <<EOF
-    <VirtualHost *:80>
+    <VirtualHost *:8060>
       DocumentRoot "/var/www/app/public"
       ServerName example.local
       ServerAlias www.example.local
@@ -63,14 +63,83 @@ EOF
 )
 echo "${VHOST}" > /etc/apache2/sites-enabled/000-default.conf
 
+# change apache default port
+sed -i 's|80|8080|g' /etc/apache2/ports.conf
+
 # Loading needed modules to make apache work
 a2enmod actions fastcgi rewrite 
 sudo service apache2 restart
 
 
+# ---------------------------------------------------------------------------------------------------------------------
+echoTitle "Install and configure NGINX"
+# ---------------------------------------------------------------------------------------------------------------------
+
+apt-get install -y nginx >> /vagrant/vm_build.log 2>&1
+sudo rm /etc/nginx/sites-enabled/default
+
+# Setup nginx conf file
+CONF=$(cat <<EOF
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server ipv6only=on;
+
+	root /var/www/static;
+	index index.html index.htm;
+
+	# Make site accessible from http://localhost/
+	server_name localhost;
+
+	location / {
+		# First attempt to serve request as file, then
+		# as directory, then fall back to displaying a 404.
+		try_files $uri $uri/ =404;
+		# Uncomment to enable naxsi on this location
+		# include /etc/nginx/naxsi.rules
+	}
+
+	# Only for nginx-naxsi used with nginx-naxsi-ui : process denied requests
+	#location /RequestDenied {
+	#	proxy_pass http://127.0.0.1:8080;    
+	#}
+
+	#error_page 404 /404.html;
+
+	# redirect server error pages to the static page /50x.html
+	#
+	#error_page 500 502 503 504 /50x.html;
+	#location = /50x.html {
+	#	root /usr/share/nginx/html;
+	#}
+
+	# pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+	#
+	#location ~ \.php$ {
+	#	fastcgi_split_path_info ^(.+\.php)(/.+)$;
+	#	# NOTE: You should have "cgi.fix_pathinfo = 0;" in php.ini
+	#
+	#	# With php5-cgi alone:
+	#	fastcgi_pass 127.0.0.1:9000;
+	#	# With php5-fpm:
+	#	fastcgi_pass unix:/var/run/php5-fpm.sock;
+	#	fastcgi_index index.php;
+	#	include fastcgi_params;
+	#}
+
+	# deny access to .htaccess files, if Apache's document root
+	# concurs with nginx's one
+	#
+	#location ~ /\.ht {
+	#	deny all;
+	#}
+}
+EOF
+)
+echo "${CONF}" >  /etc/nginx/sites-enabled/example.local
+sudo service nginx restart
 
 # ---------------------------------------------------------------------------------------------------------------------
-# echoTitle 'MYSQL-Database'
+echoTitle 'MYSQL-Database'
 # ---------------------------------------------------------------------------------------------------------------------
 # Setting MySQL (username: root) ~ (password: password)
 sudo debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password password password'
